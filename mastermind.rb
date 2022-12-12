8d4f6e313b46021c9242b83d5df4b4d479892543
Matt Turner <mattturner1911@gmail.com>
2022-12-12 08:08:06 -0500

    Added colorize gem for colored output. Started with comments to roughly draft out the game process.

---
+++ mastermind.rb
@@ -0,0 +1,9 @@
+require 'colorize'
+# Game will be played between a Player and a Computer
+# Computer will randomly select a color pattern
+# Colors will be associated with numbers 1-6, with White and Black reserved for feedback
+# Player will have twelve rounds to guess pattern
+# If the Player guesses the code correctly the game ends with the Player winning
+# If the Player cannot guess the code correctly, in twelve rounds, the Player loses
+
+puts "  ".on_blue
